fa4271afe7827db55d21d5e64a62990de242a548
asyl <narmatov.asylbek@gmail.com>
2023-01-04 15:03:24 +0600

    coulnt solve maxomum sum of non overlapping arrays

---
+++ dynamic_programming/maximum_sum_of_two_non_overlapping_arrays.py
@@ -0,0 +1,32 @@
+
+
+def solution(nums: list[int], fl: int, sl: int) -> int:
+    """
+    >>> solution([0,6,5,2,2,5,1,9,4], 1, 2)
+    20
+    
+    >>> solution([3,8,1,3,2,1,8,9,0], 3, 2)
+    29
+    
+    >>> solution([2,1,5,6,0,9,5,0,3,8], 4, 3)
+    31
+    """
+    
+    def helper(i: int, k: int) -> int:
+        if i + k >= len(nums):
+            return 0
+        
+    
+    def find_max(i: int, fl: int, sl: int) -> int:
+        if i + fl >= len(nums) or i + sl >= len(nums):
+            return 0
+        
+        first = sum(nums[i : fl]) + find_max(i + fl, fl, sl)
+        second = sum(nums[i : sl]) + find_max(i + sl, fl, sl)
+        without = find_max(i + 1, fl, sl)
+        return max(first, second, without)
+    return find_max(0, fl, sl)
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
